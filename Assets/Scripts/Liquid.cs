7c5bc9871757f468341912f4bc1ec247843ab121
GRMv1 <64690277+GRMv1@users.noreply.github.com>
2021-11-07 17:14:39 +0100

    liquidHUD

--- Assets/Scripts/Liquid.cs
+++ Assets/Scripts/Liquid.cs
@@ -6,11 +6,19 @@ using UnityEngine.UI;
 
 public class Liquid : MonoBehaviour
 {
-    Text massValue;
+    float massValue = 2;
+    float t1Value = 1;
+    float t2Value = 21;
+
+    Text massValueTxt;
+    Text t1ValueTxt;
+    Text t2ValueTxt;
     // Start is called before the first frame update
     void Start()
     {
-        massValue = GetComponent<Text>();
+        massValueTxt = GetComponent<Text>();
+        t1ValueTxt = GetComponent<Text>();
+        t2ValueTxt = GetComponent<Text>();
     }
 
     // Update is called once per frame
@@ -21,6 +29,17 @@ public class Liquid : MonoBehaviour
 
     public void ChangeMassValue(float value)
     {
-        massValue.text = value.ToString();
+        massValue = value;
+        massValueTxt.text = value.ToString();
+    }
+    public void ChangeT1Value(float value)
+    {
+        t1Value = value;
+        t1ValueTxt.text = value.ToString();
+    }
+    public void ChangeT2Value(float value)
+    {
+        t2Value = value;
+        t2ValueTxt.text = value.ToString();
     }
 }
