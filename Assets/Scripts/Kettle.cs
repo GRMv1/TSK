bbc4f515d924934fa1b39499f3c078bdfc6e7d65
VaRoXXas <kmichniewski@gmail.com>
2021-11-07 20:12:15 +0100

    Added water

--- Assets/Scripts/Kettle.cs
+++ Assets/Scripts/Kettle.cs
@@ -8,11 +8,25 @@ public class Kettle : MonoBehaviour
     float powerValue = 500;
     float efficiencyValue = 1;
 
+    public int waterDropNumber = 0;
+
     Text powerValueTxt;
     Text efficiencyValueTxt;
+    GameObject water;
+    Transform newTransform;
     // Start is called before the first frame update
     void Start()
     {
+        
+        water = this.gameObject.transform.GetChild(0).gameObject;
+        newTransform = this.gameObject.transform;
+        water.transform.localScale = new Vector3(0.25f, 0.25f,1);
+        for (int i = 0; i< waterDropNumber; i++)
+        {
+            Instantiate(water, newTransform.position, newTransform.rotation, newTransform);
+        }
+            
+
         powerValueTxt = GetComponent<Text>();
         efficiencyValueTxt = GetComponent<Text>();
     }
