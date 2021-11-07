28949fc634e43e3ae42ca09eb0e2a5b4add00b21
GRMv1 <64690277+GRMv1@users.noreply.github.com>
2021-11-07 16:10:39 +0100

    textmeshpro added

--- Assets/Scripts/Liquid.cs
+++ Assets/Scripts/Liquid.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Liquid : MonoBehaviour
 {
+    TextMeshPro massValue;
     // Start is called before the first frame update
     void Start()
     {
-        
+        massValue = GetComponent<TextMeshPro>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void ChangeMassValue(float value)
+    {
+        massValue.text = value.ToString();
     }
 }
